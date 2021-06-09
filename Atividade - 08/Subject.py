e15141e63a82341894cbfcf879a70e66390c1b84
Wadrian Araujo de Moura <wadm.snf20@uea.edu.br>
2021-06-09 18:02:47 -0400

    add class Subject

--- Atividade - 08/Subject.py
+++ Atividade - 08/Subject.py
@@ -1,5 +1,15 @@
+import Professor
+
 class Subject:
     def __init__(self, name, code):
         self.name = name
         self.code = code
-        self.professors = []
+
+    def lancamento_notas(self,professors):
+        for cont in professors:
+            print(cont.name, "lançando notas de", self.name, "({})".format(self.code))
+
+
+s = Subject("laboratorio", 12345)
+p = Professor.Professor("jucimano")
+s.lancamento_notas([p])
