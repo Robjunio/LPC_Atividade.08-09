2dba0e643e2fa7680eb2b510e27b914d62776c46
Wadrian Araujo de Moura <wadm.snf20@uea.edu.br>
2021-06-14 17:55:51 -0400

    a

--- Atividade - 08/Subject.py
+++ Atividade - 08/Subject.py
@@ -9,8 +9,3 @@ class Subject:
     def lancamento_notas(self,professors):
         for cont in professors:
             print(cont.name, "lançando notas de", self.name, "({})".format(self.code))
-
-
-s = Subject("laboratorio", 12345)
-p = Professor.Professor("jucimano")
-s.lancamento_notas([p])
